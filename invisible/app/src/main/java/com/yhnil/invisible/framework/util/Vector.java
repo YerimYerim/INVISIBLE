fa23f2652d1c67d70e0819882039ae468ec1633c
YerimYerim <vigvang_bip@naver.com>
2020-06-10 23:09:58 +0900

    joystick fix

---
+++ invisible/app/src/main/java/com/yhnil/invisible/framework/util/Vector.java
@@ -0,0 +1,11 @@
+package com.yhnil.invisible.framework.util;
+
+public class Vector {
+   public float x;
+   public float y;
+
+   public Vector(float x, float y){
+       this.x = x;
+       this.y = y;
+   }
+}
