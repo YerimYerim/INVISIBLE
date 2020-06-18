d340eac8b1c3fdd079f251fab5c9097f901c857e
YerimYerim <vigvang_bip@naver.com>
2020-06-18 21:30:39 +0900

    Add CircleColidable

---
+++ invisible/app/src/main/java/com/yhnil/invisible/framework/iface/CircleCollidable.java
@@ -0,0 +1,7 @@
+package com.yhnil.invisible.framework.iface;
+
+import com.yhnil.invisible.framework.util.Vector;
+
+public interface CircleCollidable {
+    public void getCircle(Vector point ,  float R);
+}
